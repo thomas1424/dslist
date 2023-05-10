90f54808550b1f5adcefdfd8dfea31fcd3856a71
Thomas Paulo Jr <thomaspaulo@hotmail.com>
2023-05-09 21:30:34 -0300

    Database seed update

--- src/main/java/com/devsuperior/dslist/entities/BelongingPK.java
+++ src/main/java/com/devsuperior/dslist/entities/BelongingPK.java
@@ -14,7 +14,7 @@ public class BelongingPK {
 	private Game game;
 	
 	@ManyToOne
-	@JoinColumn(name = "lista_id")
+	@JoinColumn(name = "list_id")
 	private GameList list;
 	
 	public BelongingPK() {		
